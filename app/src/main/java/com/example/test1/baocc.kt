d73f49670dbae295950abb248ba32f4116c934d5
ninhdt289 <ninhdt@apero.vn>
2025-01-17 11:45:04 +0700

    accccc

--- app/src/main/java/com/example/test1/baocc.kt
+++ app/src/main/java/com/example/test1/baocc.kt
@@ -4,6 +4,10 @@ class baocc {
 }
 fun main(){
     println("nguyen khac hang con cho an cut")
+    println("nguyen khac hang con cho an cut")
+    println("nguyen khac hang con cho an cut")
+    println("nguyen khac hang con cho an cut")
+    println("nguyen khac hang con cho an cut")
 }
 
 fun multiple(a: Int, b: Int): Int = a /b
