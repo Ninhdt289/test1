14f1486d0f29cf2e28d073d46c4492b4c56350a5
baong24 <BaoNG@apero.vn>
2025-01-17 11:44:45 +0700

    conflict 2

--- app/src/main/java/com/example/test1/baocc.kt
+++ app/src/main/java/com/example/test1/baocc.kt
@@ -1,9 +1,11 @@
 package com.example.test1
 
-class baocc {
-}
+class baocc(val name: String)
 fun main(){
     println("nguyen khac hang con cho an cut")
+    val a = baocc("khac hoang ngu")
+    multiple(1,1)
+    sum(9,10)
 }
 
 fun multiple(a: Int, b: Int): Int = a /b
