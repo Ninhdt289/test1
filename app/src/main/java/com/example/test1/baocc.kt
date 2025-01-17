ba8eb3cc0891842c03458395edb2adf41a5d44b3
baong24 <BaoNG@apero.vn>
2025-01-17 11:30:59 +0700

    conflict

--- app/src/main/java/com/example/test1/baocc.kt
+++ app/src/main/java/com/example/test1/baocc.kt
@@ -1,7 +1,13 @@
 package com.example.test1
 
-class baocc {
-}
+class baocc( val name: String)
 fun main(){
     println("baocc")
+    multiple(1,9)
+    multiple(9,1)
+    sum(9,1)
+    val a = baocc("ninhnguuluat")
 }
+fun multiple(a: Int, b: Int): Int = a /b
+
+fun sum(a: Int, b: Int): Double = a.toDouble() * b
